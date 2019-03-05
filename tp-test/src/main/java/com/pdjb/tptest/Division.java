31356c750d8e59d1e85f1c4f206a80eca85fa7fd
Pierre deffay <p.deffay@iia-laval.fr>
2019-03-05 22:25:51 +0100

    added tests for both methods

--- tp-test/src/main/java/com/pdjb/tptest/Division.java
+++ tp-test/src/main/java/com/pdjb/tptest/Division.java
@@ -1,6 +1,10 @@
 package com.pdjb.tptest;
 
+import static org.junit.Assert.assertTrue;
+
 import java.util.Scanner;
+
+import org.junit.Test;
 
 public class Division {
 	Scanner scan = new Scanner(System.in); 
@@ -11,8 +15,20 @@ public class Division {
 		a = scan.nextDouble();
 		System.out.println("Entrez une deuxième valeur");
 		b = scan.nextDouble();
+		double val = diviser(a, b);
+
+		return val;
+	}
+	
+	public double diviser(double a, double b) {
 		double val = a / b;
 
 		return val;
-}
+	}
+	
+	@Test
+	 public void testDiviser() {
+		 Division division = new Division();
+        assertTrue(division.diviser(6,2) == 3);
+  }
 }
--- tp-test/src/main/java/com/pdjb/tptest/Multiplication.java
+++ tp-test/src/main/java/com/pdjb/tptest/Multiplication.java
@@ -1,6 +1,9 @@
 package com.pdjb.tptest;
 
 import java.util.Scanner;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class Multiplication {
 	Scanner scan = new Scanner(System.in); 
@@ -11,8 +14,20 @@ public class Multiplication {
 		a = scan.nextDouble();
 		System.out.println("Entrez une deuxième valeur");
 		b = scan.nextDouble();
+		double val = mutliplier(a, b);
+
+		return val;
+	}
+	
+	public double mutliplier(double a, double b) {
 		double val = a * b;
 
 		return val;
 	}
+	
+	@Test
+	 public void testMultiplier() {
+		 Multiplication multiplication = new Multiplication();
+         assertTrue(multiplication.mutliplier(2,2) == 4);
+   }
 }
