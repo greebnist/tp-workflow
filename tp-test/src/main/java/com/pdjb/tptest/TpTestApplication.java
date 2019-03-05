2f802ab780908ba9a8fc6bcda0d8a7a83c05901e
Pierre deffay <p.deffay@iia-laval.fr>
2019-03-05 16:52:20 +0100

    changed configuration file for gradle

--- tp-test/src/main/java/com/pdjb/tptest/TpTestApplication.java
+++ tp-test/src/main/java/com/pdjb/tptest/TpTestApplication.java
@@ -1,13 +1,9 @@
 package com.pdjb.tptest;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class TpTestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TpTestApplication.class, args);
+		
 	}
 
 }
