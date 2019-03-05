211625f4b654551f6def89d75c04ee5e69dfd751
Pierre deffay <p.deffay@iia-laval.fr>
2019-03-05 19:11:35 +0100

    added menu

--- tp-test/src/main/java/com/pdjb/tptest/TpTestApplication.java
+++ tp-test/src/main/java/com/pdjb/tptest/TpTestApplication.java
@@ -1,9 +1,21 @@
 package com.pdjb.tptest;
+import java.util.Scanner;
 
 public class TpTestApplication {
-
+	
+	Scanner scan = new Scanner(System.in); 
+	
 	public static void main(String[] args) {
-		
+		TpTestApplication calculator = new TpTestApplication();
+		Scanner scan = new Scanner(System.in);
+		calculator.showMenu();
+		int x = scan.nextInt();
 	}
 
+	public void showMenu() {
+		System.out.println("1.Addition");
+		System.out.println("2.Soustraction");
+		System.out.println("3.Multiplication");
+		System.out.println("4.Division");
+	}
 }
